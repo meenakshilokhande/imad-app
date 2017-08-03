e4cd87823f9bfa6723bce8bacb04e4fca713c129
meenakshi <30298221+meenakshilokhande@users.noreply.github.com>
2017-08-04 01:52:09 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -15,7 +15,7 @@ submit.onclick=function(){
     //make the req to the server and send the name
  //capture a list of names and render it as a list 
    var names=['name1','name2','name3','name4'];
-   var list='';
+   var list = '' ;
    for(var i=0; i<names.length;i++){
        list+='<li>'+names[i]+'</li>';
        }
