42bc7aa931063d27b23e405fa2d97519ed96a8d8
meenakshi <30298221+meenakshilokhande@users.noreply.github.com>
2017-08-03 19:08:15 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,7 +6,7 @@ var element=document.getElementById('main-text');
 element.innerHTML='click on image';
 
 // move an image
-var img=document.getElementById('img');
+var img=document.getElementById('madi');
 img.onclick=function(){
     img.style=marginileft='100px';
 };
